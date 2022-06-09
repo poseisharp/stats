0acd290268d336eaf3824bc4ba750c9a152376da
Posei <55609449+poseisharp@users.noreply.github.com>
2022-06-09 11:32:32 +0700

    Update github_stats.py

--- github_stats.py
+++ github_stats.py
@@ -377,9 +377,7 @@ Languages:
 
         # TODO: Improve languages to scale by number of contributions to
         #       specific filetypes
-        print(type(self._languages))
-        newIndex = sorted(self._languages, key=lambda x: self._languages[x]['size'])
-        self._languages = {newIndex[k]: self._languages[k] for k in newIndex}
+        print(self._languages)
         self._languages = dict(itertools.islice(self._languages.items(), 5))
         langs_total = sum([v.get("size", 0) for v in self._languages.values()])
         for k, v in self._languages.items():
